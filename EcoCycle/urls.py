f91bd35ae305116e76dcaf6d9f6b811a2357f2a4
Chaimaa Elfarji <c.elfarji@yanvision.ma>
2024-05-18 14:26:13 +0100

    requirements

--- EcoCycle/urls.py
+++ EcoCycle/urls.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
 
     path('api/', include('Api_app.urls')),
-    path('', include('rest_auth.urls')),
+    path('', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
